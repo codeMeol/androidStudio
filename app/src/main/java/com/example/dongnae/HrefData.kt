74dd88ce4d37efa64c164237a793b63bdcde783f
codeMeol <hjy7485@gmail.com>
2022-07-03 10:05:39 +0900

    first-commit

---
+++ app/src/main/java/com/example/dongnae/HrefData.kt
@@ -0,0 +1,14 @@
+package com.example.dongnae
+
+class HrefData {
+    val liTag_1 : String = "언니옷가게"
+    val liTag_2 : String = "애들옷가게"
+    val liTag_3 : String = "코스메틱"
+    val liTag_4 : String = "다이어트"
+    val liTag_5 : String = "오빠옷가게"
+    val liTag_6 : String = "구디백세트"
+    val liTag_7 : String = "이벤트"
+    val liTag_8 : String = "SALE"
+
+
+}
